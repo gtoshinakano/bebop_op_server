b4eb58042ebb6a249d459d1d3b7a275d04571887
Nakano Toshinori <gtoshinakano@gmail.com>
2020-07-16 15:39:41 +0900

    Added battery namespace to emit battery levels to client

--- server.js
+++ server.js
@@ -5,22 +5,25 @@ var jsoncolor = require('json-colorizer');
 io.on('connect', socket => {
   console.log('Client connected to Socket.io');
   socket.emit("ping", "pong");
+  socket.emit("drone_connected", false)
+
+  //Init drone
+  var drone = bebop.createClient();
+
+  drone.connect(function() {
+    console.log("Bebop is ready to fly!");
+    drone.on("ready", () => {
+      socket.emit("drone_connected", true)
+      console.log(drone.navData);
+    })
+
+    drone.on("battery", (data) => socket.emit("battery_level", data))
+
+
+
+  })
 
   socket.on('disconnect', () => {
     console.log(jsoncolor({text: 'Client diconnected from Socket.io'}));
   })
 });
-
-
-//var drone = bebop.createClient();
-
-/* drone.connect(function() {
-  console.log("Conectei e vou subir");
-  drone.takeOff();
-
-  setTimeout(function() {
-    console.log("To descendo mulek");
-    drone.land();
-  }, 5000);
-});
-*/
